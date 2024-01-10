20dd1fe768f121ae262c7fc6c583e2aaf1d39f75
Narmin-G-M <narmin.g.mammadli@gmail.com>
2024-01-10 17:57:43 +0400

    Toast Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
-import { Flex,HStack,Heading, Spacer,Box, Text,Button } from '@chakra-ui/react'
+import { UnlockIcon } from '@chakra-ui/icons';
+import { Flex,HStack,Heading, Spacer,Box, Text,Button, useToast } from '@chakra-ui/react'
 import React from 'react'
 
 export default function Navbar() {
+
+  const toast = useToast();
+
+  const showToast = () => {
+    toast({
+      title: 'Logged out',
+      description: 'Successfully logged out',
+      duration: 5000,
+      isClosable: true,
+      status:'success',
+      position: 'top',
+      icon: <UnlockIcon />
+    })
+  }
+
   return (
     <Flex  as='nav' mb='40px' p='10px' alignItems='center'>
         <Heading as='h1'>Task</Heading>
@@ -10,7 +26,7 @@ export default function Navbar() {
         <HStack spacing='50px'>
           <Box bg='gray.200' p='10px'> M</Box>
           <Text>someone@gmail.com</Text>
-          <Button colorScheme='purple'>Logout</Button>
+          <Button colorScheme='purple' onClick={showToast}>Logout</Button>
         </HStack>
     </Flex>
   )
